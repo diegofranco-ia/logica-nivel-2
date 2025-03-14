c867fe06a6c705f702100b7bad8b272b80d19749
diegofranco-ia <diego.franco1800@gmail.com>
2025-03-13 21:52:03 -0300

    Update ex-05.py

--- ex-05.py
+++ ex-05.py
@@ -1,5 +1,5 @@
 #5) Escreva um algoritmo para ler as dimensões de um retângulo (base e altura), calcular e escrever a área do retângulo.
-print ("diga as bases do retangulo")
+print ("diga a base e altura do retangulo")
 numero1 = int(input())
 numero2 = int(input())
 print ("a area do retangulo")
